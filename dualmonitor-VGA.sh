3c2eaa855d60bf8a8059b756917a656c2873e8da
rDini <r4faeldini@gmail.com>
2014-05-14 10:44:21 -0300

    Only the usefull script on repository

--- dualmonitor-VGA.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Author: rDini
-
-if [ $(xrandr | grep -A 1 " connected " | grep -v "^--$" | awk '{print $1}' | grep -c HDMI1) ] ; then
-	cmd="xrandr --output HDMI1 --auto --left-of LVDS1"
-	echo "Running command: $cmd"
-	eval $cmd
-fi
